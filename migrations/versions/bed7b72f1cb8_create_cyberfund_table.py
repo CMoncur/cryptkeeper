48215ceefe5d7500f884633d97c2f214423ad5ff
CMoncur <cmoncur@gmail.com>
2018-03-01 12:18:42 -0800

    Migration for cyberfund table

--- migrations/versions/bed7b72f1cb8_create_cyberfund_table.py
+++ migrations/versions/bed7b72f1cb8_create_cyberfund_table.py
@@ -1,10 +1,10 @@
 # pylint: disable=E1101
 
-"""Create Cointelegraph Table
+"""Create Cyberfund Table
 
-Revision ID: 214124f99734
-Revises: 28c5323d02ba
-Create Date: 2018-03-01 12:00:18.084360
+Revision ID: bed7b72f1cb8
+Revises: 214124f99734
+Create Date: 2018-03-01 12:15:49.623039
 
 """
 
@@ -13,17 +13,17 @@ from datetime import datetime
 
 # Other Dependencies
 from alembic import op
-from sqlalchemy import Column, Integer, String, TEXT, TIMESTAMP
+from sqlalchemy import BOOLEAN, Column, FLOAT, Integer, String, TEXT, TIMESTAMP
 
 
 # Revision identifiers, used by Alembic.
-revision = '214124f99734'
-down_revision = '28c5323d02ba'
+revision = 'bed7b72f1cb8'
+down_revision = '214124f99734'
 branch_labels = None
 depends_on = None
 
 # Table Name Constant
-TABLE = "cointelegraph"
+TABLE = "cyberfund"
 
 def upgrade():
   """Migration Up"""
@@ -35,7 +35,11 @@ def upgrade():
     Column( "start", TIMESTAMP, nullable = False ),
     Column( "end", TIMESTAMP, nullable = False ),
     Column( "site", String(100), nullable = False ),
-    Column( "description", TEXT, nullable = False )
+    Column( "description", TEXT, nullable = False ),
+    Column( "price", FLOAT(20), nullable = False ),
+    Column( "presale_start", TIMESTAMP, nullable = False ),
+    Column( "presale_end", TIMESTAMP, nullable = False ),
+    Column( "token_symbol", String(20), nullable = False )
   )
 
 
