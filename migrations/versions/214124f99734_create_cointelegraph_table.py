e4af46320b8e1f2983134c585f5732d762c0334d
CMoncur <cmoncur@gmail.com>
2018-03-01 12:09:57 -0800

    Migration for cointelegraph table

---
+++ migrations/versions/214124f99734_create_cointelegraph_table.py
@@ -0,0 +1,44 @@
+# pylint: disable=E1101
+
+"""create cointelegraph table
+
+Revision ID: 214124f99734
+Revises: 28c5323d02ba
+Create Date: 2018-03-01 12:00:18.084360
+
+"""
+
+# Core Dependencies
+from datetime import datetime
+
+# Other Dependencies
+from alembic import op
+from sqlalchemy import Column, Integer, String, TEXT, TIMESTAMP
+
+
+# Revision identifiers, used by Alembic.
+revision = '214124f99734'
+down_revision = '28c5323d02ba'
+branch_labels = None
+depends_on = None
+
+# Table Name Constant
+TABLE = "cointelegraph"
+
+def upgrade():
+  """Migration Up"""
+  op.create_table(
+    TABLE,
+    Column( "id", Integer, primary_key = True, autoincrement = True ),
+    Column( "created", TIMESTAMP, nullable = False, default = datetime.now ),
+    Column( "name", String(100), nullable = False ),
+    Column( "start", TIMESTAMP, nullable = False ),
+    Column( "end", TIMESTAMP, nullable = False ),
+    Column( "site", String(100), nullable = False ),
+    Column( "description", TEXT, nullable = False ),
+  )
+
+
+def downgrade():
+  """Migration Down"""
+  op.drop_table(TABLE)
