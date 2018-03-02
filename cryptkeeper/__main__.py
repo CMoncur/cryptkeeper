596e1706e56805de66460d7809d8613bfe11cbcf
CMoncur <cmoncur@gmail.com>
2018-03-02 15:31:26 -0800

    Base IcoDrops excavator

--- cryptkeeper/__main__.py
+++ cryptkeeper/__main__.py
@@ -1,10 +1,16 @@
 """ Cryptkeeper Entry Point """
 
+# Core Dependencies
 import sys
+
+# Internal Dependencies
+from quarry.node.icodrops import IcoDrops
 
 def main():
   """ Entry function """
-  print(sys.argv)
+  urls = [ sys.argv[1] ] * 1000
+  hello = IcoDrops(urls)
+  print(hello.errors())
 
 if __name__ == "__main__":
   main()
---
+++ cryptkeeper/quarry/node/icodrops.py
@@ -0,0 +1,15 @@
+""" ICODrops Excavator """
+
+# External Dependencies
+# from bs4 import BeautifulSoup
+
+# Internal Dependencies
+from ..excavator import Excavator
+
+
+# Public Entities
+class IcoDrops(Excavator):
+  """ ICODrops Excavator Class """
+
+  def __init__(self, urls):
+    super(IcoDrops, self).__init__(urls, True)
